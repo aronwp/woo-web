7c8678bbd652dd20134815352361a4e9bff94328
Aron <aronwp@gmail.com>
2013-09-24 02:03:30 -0500

    cleaned up some code

--- wooweb.php
+++ wooweb.php
@@ -62,9 +62,9 @@ add_action('admin_head', 'remove_upgrade_nag');
 //Automatically spam comments with a very long url
   function rkv_url_spamcheck( $approved , $commentdata ) {
     return ( strlen( $commentdata['comment_author_url'] ) > 50 ) ? 'spam' : $approved;
-  }
+}
 
-  add_filter( 'pre_comment_approved', 'rkv_url_spamcheck', 99, 2 );
+add_filter( 'pre_comment_approved', 'rkv_url_spamcheck', 99, 2 );
 
 
 
